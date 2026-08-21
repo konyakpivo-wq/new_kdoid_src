01b10c8cea44abfe10165e40ca9d258d733c7668
konyakpivo-wq <konyakpivo@yandex.com>
2026-08-21 23:52:42 +0200

    Fix initial activity dependency

--- app/src/main/java/com/newkdroid/app/MainActivity.java
+++ app/src/main/java/com/newkdroid/app/MainActivity.java
@@ -1,11 +1,10 @@
 package com.newkdroid.app;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
